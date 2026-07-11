2d1240bd86d46c139e847bb0e49c4f582427ad2f
StanislavSol <vandemar@bk.ru>
2026-07-11 13:06:21 +0300

    refact

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,11 @@
 <?php
 
 use App\Http\Controllers\RedirectController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/dashboard', function () {
-    return redirect('/admin');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 Route::get('/{code}', [RedirectController::class, 'redirect'])
     ->where('code', '[A-Za-z0-9]+');
-
-require __DIR__.'/auth.php';
